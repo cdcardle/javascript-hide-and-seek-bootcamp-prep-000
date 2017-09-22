50703d479ceb2bc56572eb7b46442cd61a86f40a
Chris Cardle <cdcardle@gmail.com>
2017-09-22 13:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
 function nestedTarget() {
   return document.querySelectorAll('.target')
 }
+
+function increaseRankBy(n) {
+  const list = dpcument.getElementsByClassName('ranked')
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (i + 1).toString()
+  }
+}
